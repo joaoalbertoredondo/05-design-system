47a2113a57a74a4ab2f9d6529eef7e5e13c45420
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2024-09-06 09:04:03 -0300

    fix: main.js

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,16 +1,16 @@
 import { join, dirname } from "path";
-import { config } from "process";
+import { config as processConfig } from "process"; // Renomeado para evitar conflito
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, "package.json"))); // Verifique se está usando CommonJS ou ES Modules
 }
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
+const storybookConfig = {
   stories: [
     "../src/pages/**/*.mdx",
     "../src/stories/**/*.stories.tsx",
@@ -32,7 +32,8 @@ const config = {
       config.base = "/05-design-system/"
     }
 
-    return config
+    return config;
   }
 };
-export default config;
+
+export default storybookConfig;
