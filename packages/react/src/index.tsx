cc7895a734ffc1f8f5985bfed4fce585839a5eb4
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2024-09-09 16:05:10 -0300

    desafio: component tooltip

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -8,5 +8,6 @@ export * from "./components/TextArea";
 export * from "./components/Checkbox";
 export * from "./components/MuiltiStep";
 export * from "./components/Toast";
+export * from "./components/ToolTip";
 
 export * from "./styles";
